29f56beb113d1f6f38c11618cfdc55b218f6eb11
NingM <semsociaty@gmail.com>
2020-02-18 11:33:52 +0100

    changes

--- word_visual_search/scripts/plot_RT.py
+++ word_visual_search/scripts/plot_RT.py
@@ -14,17 +14,29 @@ import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
 
+sub_name = 'pilot'
 working_dir = '../results/'
-working_data = glob(os.path.join(working_dir,'*.csv'))
+saving_dir = '../result_figures'
+if not os.path.exists(os.path.join(saving_dir,sub_name)):
+    os.mkdir(os.path.join(saving_dir,sub_name))
+working_data = glob(os.path.join(working_dir,sub_name,'*.csv'))
 
 fig,axes = plt.subplots(figsize = (8,12),
-                        nrows = 3)
+                        nrows = 3,
+                        sharex = True,
+                        sharey = True,
+                        )
 for f,ax in zip(working_data,axes.flatten()): 
     df = pd.read_csv(f)
     ax = sns.barplot(x = 'set_size',
                      y = 'just_press.rt',
                      data = df,
                      ax = ax,
+                     seed = 12345,
                      )
-    
+    ax.set(xlabel = 'Set Size',
+           ylabel = 'Reaction Time (sec)')
+fig.savefig(os.path.join(saving_dir,sub_name,'results.jpeg'),
+            dpi = 300,
+            bbox_inches = 'tight')
     
